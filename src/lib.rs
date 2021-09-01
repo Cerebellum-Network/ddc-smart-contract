e8a2742f39b535d26ddd105744c608c2e30d0318
Evgeny Svirsky <evgeny@cere.io>
2021-09-01 13:11:35 +0200

    * Remove unused var

--- src/lib.rs
+++ src/lib.rs
@@ -761,9 +761,6 @@ mod ddc {
         }
     }
 
-    // ---- DDC nodes ----
-    const DDC_NODE_PERMISSION_TRUSTED: u64 = 1;
-
     #[derive(
         Default, Clone, PartialEq, Eq, PartialOrd, Ord, Encode, Decode, SpreadLayout, PackedLayout,
     )]
