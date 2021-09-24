106629585577789636f09acdfdc678fc22c719fb
tabakopavel <pavel.tabako@gmail.com>
2021-09-24 10:28:57 +0200

    Add Integration test fro deploy smart contract on latests pos-network-node version (#60)
    - add integration tests
    - add script for running integration tests
    - update README.md

---
+++ scripts/stop-infrastructure.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Kill all containers
+docker kill $(docker ps -q)
+docker rm -f $(docker ps -a -q)
+
+# Delete chain-data folder if exist
+[ -d "pos-node/chain-data1" ] && rm -r "pos-node/chain-data1"
+[ -d "pos-node/chain-data2" ] && rm -r "pos-node/chain-data2"
+
