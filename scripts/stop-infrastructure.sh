40b94d4124aa9318b81b0e479842015d344c6234
Pavel Tabako <pavel.tabako@minsk.leveris.com>
2021-09-24 09:55:08 +0200

    - fixes after review

--- scripts/stop-infrastructure.sh
+++ scripts/stop-infrastructure.sh
@@ -4,3 +4,7 @@
 docker kill $(docker ps -q)
 docker rm -f $(docker ps -a -q)
 
+# Delete chain-data folder if exist
+[ -d "pos-node/chain-data1" ] && rm -r "pos-node/chain-data1"
+[ -d "pos-node/chain-data2" ] && rm -r "pos-node/chain-data2"
+
