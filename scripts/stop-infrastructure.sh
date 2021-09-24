9dedb3bde4f33989ed3739c48a9170c8f82033e9
tabakopavel <pavel.tabako@gmail.com>
2021-09-24 11:55:24 +0200

    Add workflow for running integration tests on pull request (#61)

--- scripts/stop-infrastructure.sh
+++ scripts/stop-infrastructure.sh
@@ -5,6 +5,8 @@ docker kill $(docker ps -q)
 docker rm -f $(docker ps -a -q)
 
 # Delete chain-data folder if exist
+sudo chmod -R 777 "pos-node/chain-data1"
+sudo chmod -R 777 "pos-node/chain-data2"
 [ -d "pos-node/chain-data1" ] && rm -r "pos-node/chain-data1"
 [ -d "pos-node/chain-data2" ] && rm -r "pos-node/chain-data2"
 
